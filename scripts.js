9685fa3b95bb5258250fce1b5f9a951e4e632494
krisztian <krisztian.gombos@jibecompany.com>
2025-06-18 13:26:31 +0300

    add cv

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+document.querySelectorAll('.section, .project-card').forEach(section => {
+  observer.observe(section);
+});
